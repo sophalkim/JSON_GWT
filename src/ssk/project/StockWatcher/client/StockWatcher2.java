5c4764cffbe1dab5f343e101927b9903e334fbea
sophalkim <sophal.kim.work@gmail.com>
2014-10-23 14:20:05 -0700

    Starting JSon

---
+++ src/ssk/project/StockWatcher/client/StockWatcher2.java
@@ -0,0 +1,41 @@
+package ssk.project.StockWatcher.client;
+
+import java.util.ArrayList;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class StockWatcher2 implements EntryPoint {
+
+	private static final int REFRESH_INTERVAL = 5000;
+	
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private FlexTable stocksFlexTable = new FlexTable();
+	private HorizontalPanel addPanel = new HorizontalPanel();
+	private TextBox newSymbolTextBox = new TextBox();
+	private Button addStockButton = new Button("Add");
+	private Label lastUpdatedLabel = new Label();
+	private ArrayList<String> stocks = new ArrayList<String>();
+	private StockPriceServiceAsync stockPriceSvc = GWT.create(StockPriceService.class);
+	private Label errorMsgLabel = new Label();
+	
+	@Override
+	public void onModuleLoad() {
+		
+		// Create Table for stock data.
+		stocksFlexTable.setText(0, 0, "Symbol");
+		stocksFlexTable.setText(0, 1, "Price");
+		stocksFlexTable.setText(0, 2, "Change");
+		stocksFlexTable.setText(0, 3, "Change");
+		
+		
+	}
+
+	
+}
